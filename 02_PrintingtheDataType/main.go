cb4899238d6b4dcff5318230c434b91c7359a5a3
Yasasvi Yeleswarapu <yash0690@gmail.com>
2016-02-15 02:55:42 -0800

    print

---
+++ 02_PrintingtheDataType/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+<<<<<<< HEAD
+	var num int = 10
+=======
+	var num int = 4623
+>>>>>>> origin/master
+	var str string = "Yellapati"
+	var b = true
+
+	fmt.Printf("%T \n", num)
+	fmt.Printf("%T \n", str)
+	fmt.Printf("%T \n", b)
+
+}
+
